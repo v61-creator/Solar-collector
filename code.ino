0cdb65d22f15949cb07e9afe81fa50b6783db46f
v61-creator <v6179919@gmail.com>
2026-03-06 11:53:26 +0300

    Update code.ino

--- code.ino
+++ code.ino
@@ -1,7 +1,7 @@
 #include <Servo.h>
 #include <OneWire.h>
 #include <DallasTemperature.h>
-#include <virtuabotixRTC.h>
+#include <iarduino_RTC.h>
 #include <EEPROM.h>
 
 #define SERVO_VERT_PIN 5  
@@ -9,8 +9,8 @@
 #define ONE_WIRE_BUS   9  
 #define WIND_PIN       A6
 
-// Пины для DS1302: CLK, DAT, RST
-virtuabotixRTC myRTC(5, 4, 3); 
+// RST=5, CLK=4, DAT=3
+iarduino_RTC time(RTC_DS1302, 5, 4, 3);
 
 const float latitude = 55.75;
 const float longitude = 37.61;
@@ -36,34 +36,27 @@ String lastCriticalMode = "";
 
 void setup() {
   Serial.begin(9600);
+  time.begin();
+  
+  // Раскомментировать для настройки времени (сек, мин, час, день, мес, год, день_нед):
+  // time.settime(0, 30, 12, 6, 3, 26, 6);
   
   servoVert.attach(SERVO_VERT_PIN);
   servoHor.attach(SERVO_HOR_PIN);
-  
   servoVert.write(currentVert);
   servoHor.write(currentHor);
   
-  pinMode(WIND_PIN, INPUT);
   sensors.begin();
-  
-  // Установка времени (раскомментировать следующую строку 1 раз, чтобы установить время)
-  // myRTC.setDS1302Time(00, 30, 12, 6, 6, 3, 2026); // сек, мин, час, день_нед, день_мес, мес, год
-  
   EEPROM.get(1020, eeAddress);
   if (eeAddress < 0 || eeAddress > 1000) eeAddress = 0;
-
-  Serial.println(F("SYSTEM STARTUP (DS1302)..."));
 }
 
 void loop() {
   if (Serial.available() > 0) {
-    char cmd = Serial.read();
-    if (cmd == 'R' || cmd == 'r') dumpEEPROM();
+    if (Serial.read() == 'R') dumpEEPROM();
   }
 
-  // Обновляем время из модуля
-  myRTC.updateTime();
-
+  time.gettime();
   int lt = analogRead(A0); int rt = analogRead(A1);
   int ld = analogRead(A2); int rd = analogRead(A3);
   int wind = analogRead(WIND_PIN);
@@ -89,10 +82,8 @@ void loop() {
     isCritical = true;
   }
   else if (avgLight < 30) { 
-    
-    
     float az, el;
-    calculateSolarPosition(myRTC.hours, myRTC.minutes, myRTC.dayofmonth, myRTC.month, latitude, longitude, &az, &el);
+    calculateSolarPosition(time.Hours, time.minutes, time.day, time.month, latitude, longitude, &az, &el);
     if (el > 0) {
       targetHor = map(constrain(az, 90, 270), 90, 270, 0, 180);
       targetVert = constrain(el, 0, 90);
@@ -112,7 +103,6 @@ void loop() {
 
   if (currentVert < targetVert) currentVert++;
   else if (currentVert > targetVert) currentVert--;
-  
   if (currentHor < targetHor) currentHor++;
   else if (currentHor > targetHor) currentHor--;
 
@@ -120,18 +110,53 @@ void loop() {
   servoHor.write(currentHor);
 
   if (millis() - lastPrintTime > 300) {
-    Serial.print(F("[LIVE] L:")); Serial.print(avgLight);
+    Serial.print(F("L:")); Serial.print(avgLight);
     Serial.print(F(" | T:")); Serial.print(currentTemp, 1);
-    Serial.print(F(" | MODE:")); Serial.println(currentMode);
+    Serial.print(F(" | M:")); Serial.println(currentMode);
     lastPrintTime = millis();
   }
 
   if (isCritical && currentMode != lastCriticalMode) {
-    String logMsg = String(myRTC.hours) + ":" + String(myRTC.minutes) + " " + currentMode;
-    saveToEEPROM(logMsg);
+    saveToEEPROM(String(time.Hours) + ":" + String(time.minutes) + " " + currentMode);
     lastCriticalMode = currentMode;
   } 
   else if (!isCritical) lastCriticalMode = "";
-
   delay(40); 
+}
+
+void saveToEEPROM(String msg) {
+  for (unsigned int i = 0; i < msg.length(); i++) {
+    EEPROM.update(eeAddress, msg[i]);
+    eeAddress++;
+    if (eeAddress >= 1000) eeAddress = 0;
+  }
+  EEPROM.update(eeAddress, '\n');
+  eeAddress++;
+  if (eeAddress >= 1000) eeAddress = 0;
+  EEPROM.put(1020, eeAddress); 
+}
+
+void dumpEEPROM() {
+  for (int i = 0; i < 1000; i++) {
+    char c = EEPROM.read(i);
+    if (c != 255) Serial.print(c);
+  }
+}
+
+void calculateSolarPosition(int hr, int mn, int dy, int mo, float lat, float lon, float* az, float* el) {
+  float utcHour = hr - timezone + (mn / 60.0);
+  int N = dy + (31 * (mo - 1));
+  float declination = 23.45 * sin((360.0 / 365.0) * (N - 81) * DEG_TO_RAD);
+  float b = (360.0 / 364.0) * (N - 81);
+  float eq = 9.87 * sin(2 * b * DEG_TO_RAD) - 7.53 * cos(b * DEG_TO_RAD) - 1.5 * sin(b * DEG_TO_RAD);
+  float solarTime = utcHour + (4.0 * lon / 60.0) + (eq / 60.0);
+  float hourAngle = 15.0 * (solarTime - 12.0);
+  float latRad = lat * DEG_TO_RAD;
+  float decRad = declination * DEG_TO_RAD;
+  float hrRad = hourAngle * DEG_TO_RAD;
+  float sinEl = sin(latRad) * sin(decRad) + cos(latRad) * cos(decRad) * cos(hrRad);
+  *el = asin(constrain(sinEl, -1.0, 1.0)) * RAD_TO_DEG;
+  float cosAz = (sin(decRad) - sin(latRad) * sin(sinEl)) / (cos(latRad) * cos(asin(constrain(sinEl, -1.0, 1.0))));
+  float azRaw = acos(constrain(cosAz, -1.0, 1.0)) * RAD_TO_DEG;
+  if (hourAngle > 0) *az = 360.0 - azRaw; else *az = azRaw;
 }
